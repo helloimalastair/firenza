3d8a1644d3e7c105d81e952c3663fc83553c1817
Alastair <37487746+helloimalastair@users.noreply.github.com>
2021-12-13 21:15:30 -0800

    fix html path

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 const fs = require("fs"),
   htmlMinifier = require("html-minifier"),
-  miniHTML = html => fs.writeFileSync("./src/html/"+html,htmlMinifier.minify(fs.readFileSync("./htmlsrc/"+html).toString(),{collapseBooleanAttributes:!0,collapseInlineTagWhitespace:0,collapseWhitespace:!0,decodeEntities:!0,html5:!0,quoteCharacter:'"',removeAttributeQuotes:!0,removeComments:!0,removeEmptyAttributes:!0,removeRedundantAttributes:!0,removeStyleLinkTypeAttributes:!0,sortAttributes:!0,sortClassName:!0,useShortDoctype:!0}));
+  miniHTML = html => fs.writeFileSync("./src/html/"+html,htmlMinifier.minify(fs.readFileSync("./html/"+html).toString(),{collapseBooleanAttributes:!0,collapseInlineTagWhitespace:0,collapseWhitespace:!0,decodeEntities:!0,html5:!0,quoteCharacter:'"',removeAttributeQuotes:!0,removeComments:!0,removeEmptyAttributes:!0,removeRedundantAttributes:!0,removeStyleLinkTypeAttributes:!0,sortAttributes:!0,sortClassName:!0,useShortDoctype:!0}));
 miniHTML("FileNotFound.html");
 miniHTML("InternalError.html");
 miniHTML("HomePage.html");
